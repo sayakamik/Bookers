0ae89dc978a21f6316da940be0056dfe2dab60d6
sayakamik <138635063+sayakamik@users.noreply.github.com>
2023-08-04 09:36:24 +0000

    [Initial Commit1] Bookers

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,30 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  # 以下を追加
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    book = Book.new(book_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    redirect_to 'top'
+  end
+  
+  def index
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+  
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
