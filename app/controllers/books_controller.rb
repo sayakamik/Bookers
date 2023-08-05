e0594c8e0a8aec8d6693b270acf64b23d71c1e53
sayakamik <138635063+sayakamik@users.noreply.github.com>
2023-08-05 03:23:30 +0000

    [Initial Commit2] Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,37 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
+  def new              #フォーム表示
+    @book = Book.new   #フォーム作成
   end
 
-  # 以下を追加
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
     book = Book.new(book_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
     book.save
-    redirect_to 'top'
+    redirect_to book_path(book.id)
   end
   
   def index
     @books = Book.all
+    @book = Book.new  #indexに投稿フォームも記載のため
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト
   end
   
   private
