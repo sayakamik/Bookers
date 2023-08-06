9d5f243234dcf72e77f95650fa933c3f5d8d4381
sayakamik <138635063+sayakamik@users.noreply.github.com>
2023-08-06 02:48:13 +0000

    [Initial Commit4] Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,16 @@
 class BooksController < ApplicationController
-  def new              #フォーム表示
-    @book = Book.new   #フォーム作成
+  def top
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
   
   def index
@@ -23,15 +27,22 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @books = Book.all
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
   
   def destroy
-    book = Book.find(params[:id])  # データ（レコード）を1件取得
-    book.destroy  # データ（レコード）を削除
-    redirect_to '/books'  # 投稿一覧画面へリダイレクト
+    @book = Book.find(params[:id])  # データ（レコード）を1件取得
+    if @book.destroy  # データ（レコード）を削除
+      flash[:notice] = "Book was successfully destroyed."
+      redirect_to '/books'  # 投稿一覧画面へリダイレクト
+    end
   end
   
   private
